91a1e08e3f0e6e8f85931cd306aeb601610f899f
Kathiravan Arjunan <93112145+kat369@users.noreply.github.com>
2023-05-22 10:07:43 +0530

    Create TestPage.js

---
+++ TestPage.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+
+function TestPage() {
+
+    const [issue, setissue] = useState()
+  const data = [
+    {
+      article: "nano1",
+      issue: "issue-1",
+      year: 2016,
+    },
+    {
+      article: "mech1",
+      issue: "issue-1",
+      year: 2017,
+    },
+    {
+      article: "mech4",
+      issue: "issue-4",
+      year: 2017,
+    },
+    {
+      article: "mech44",
+      issue: "issue-4",
+      year: 2017,
+    },
+    {
+      article: "mech10",
+      issue: "issue-10",
+      year: 2017,
+    },
+    {
+      article: "aerospace22",
+      issue: "issue-2",
+      year: 2014,
+    },
+    {
+      article: "aerospace1",
+      issue: "issue-1",
+      year: 2014,
+    },
+    {
+      article: "aerospace2",
+      issue: "issue-2",
+      year: 2014,
+    },
+
+    {
+      article: "bio1",
+      issue: "issue-1",
+      year: 2015,
+    },
+    {
+      article: "bio5",
+      issue: "issue-5",
+      year: 2015,
+    },
+    {
+      article: "bio2",
+      issue: "issue-2",
+      year: 2015,
+    },
+    {
+      article: "bio22",
+      issue: "issue-2",
+      year: 2015,
+    },
+    {
+      article: "nano3",
+      issue: "issue-3",
+      year: 2016,
+    },
+    {
+      article: "nano5",
+      issue: "issue-5",
+      year: 2016,
+    },
+  ];
+
+  const yeargroup = data.reduce((datas, article) => {
+    (datas[article.year] = datas[article.year] || []).push(article);
+    return datas;
+  }, {});
+  console.log(yeargroup);
+
+  const keys = Object.keys(yeargroup);
+
+  return (
+    <>
+   
+      {keys.map((key) => {
+        const issuegroup = yeargroup[key].reduce((datas, article) => {
+          (datas[article.issue] = datas[article.issue] || []).push(article);
+          return datas;
+        }, {});
+        console.log(issuegroup);
+        const keys = Object.keys(issuegroup);
+        return(
+            console.log(),
+            keys.map((key) => {
+                issuegroup[key].map((article) => {
+                  return(
+                  console.log(article.article))
+                });
+               
+              })
+        )
+      })}
+
+<div>Test</div>
+ 
+    </>
+  );
+}
+
+export default TestPage;
